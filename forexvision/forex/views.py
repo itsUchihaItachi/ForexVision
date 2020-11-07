40987eb12c6eb4c6a57cfe6598cca2692af5a6b0
Loaderreloaded <pandeyvishal159@gmail.com>
2020-11-07 17:13:54 +0530

    Initial check

--- forexvision/forex/views.py
+++ forexvision/forex/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    name = request.POST.get('name')
+    return render(request,'home.html')
