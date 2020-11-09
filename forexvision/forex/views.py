91032c5010594871bba98f77d7cfa80ff1280974
Ravi Agrawal <raviagrawal1012@gmail.com>
2020-11-09 22:16:44 +0530

    Data passed from form to View

--- forexvision/forex/views.py
+++ forexvision/forex/views.py
@@ -3,5 +3,19 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    name = request.POST.get('name')
-    return render(request,'base.html')
+    amount = request.POST.get('amount')
+    base = request.POST.get('value')
+    counter = request.POST.get('value')
+    if amount == None :
+        amount = 0
+    return render(request,'base.html',{'amount' : amount, 'base' : base})
+
+# def page_objects(request):
+#     if request.method == 'POST':
+#         form = YourForm(request.POST)
+
+#     if form.is_valid():
+#         answer = form.cleaned_data['value']
+
+#     return render(request, 'base.html', {'answer' : answer})
+
