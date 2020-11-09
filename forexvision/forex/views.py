c5baaff1b8d1e6ab5ced11fe4550767b8735c5aa
Ravi Agrawal <raviagrawal1012@gmail.com>
2020-11-09 23:15:27 +0530

    Base and counter values

--- forexvision/forex/views.py
+++ forexvision/forex/views.py
@@ -4,18 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     amount = request.POST.get('amount')
-    base = request.POST.get('value')
-    counter = request.POST.get('value')
-    if amount == None :
-        amount = 0
-    return render(request,'base.html',{'amount' : amount, 'base' : base})
 
-# def page_objects(request):
-#     if request.method == 'POST':
-#         form = YourForm(request.POST)
+    base = request.POST.get('baseCurr')
+    counter = request.POST.get('counterCurr')
+    return render(request,'base.html',{'amount' : amount, 'baseCurr' : base, 'counterCurr' : counter})
 
-#     if form.is_valid():
-#         answer = form.cleaned_data['value']
-
-#     return render(request, 'base.html', {'answer' : answer})
 
