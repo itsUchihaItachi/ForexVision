3ac2b1c1fb0efc646909e6f4eaab635b12f88b8a
Loaderreloaded <pandeyvishal159@gmail.com>
2020-11-11 14:36:15 +0530

    before displaying amount

--- forexvision/forex/views.py
+++ forexvision/forex/views.py
@@ -4,7 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     amount = request.POST.get('amount')
-
     base = request.POST.get('baseCurr')
     counter = request.POST.get('counterCurr')
     return render(request,'base.html',{'amount' : amount, 'baseCurr' : base, 'counterCurr' : counter})
