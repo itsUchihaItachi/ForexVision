9e55d32af44db40e0a0383987d84de83ea0764d2
Ravi Agrawal <raviagrawal1012@gmail.com>
2020-11-13 11:43:34 +0530

    Last Updated added

--- forexvision/forex/urls.py
+++ forexvision/forex/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home, name = 'home')
-    # path('ExchangeCurr', views.home, name = 'home')
+    path('',views.temp, name = 'temp'),
+    path('ExchangeCurr', views.home, name = 'home')
 ]
--- forexvision/forex/views.py
+++ forexvision/forex/views.py
@@ -4,9 +4,13 @@ from urllib.request import urlopen
 import json
 from datetime import date,timedelta
 
+API_KEY = "taTjcoDno4fAXZKnSBLdvAEKonjHUq3FHdygpJiCwiRYdPKMhN"
+# vishal = taTjcoDno4fAXZKnSBLdvAEKonjHUq3FHdygpJiCwiRYdPKMhN
+# ravi = meZyuHiwLZxWD1xaBOPfkHtQx4FiWnuhQQMNxsmLQXrL12YveV
+
 # Create your views here.
-# def temp(request):
-#     return render(request,'Base.html')
+def temp(request):
+    return render(request,'Base.html')
 
 def home(request):
     amount = request.POST.get('amount')
@@ -14,68 +18,51 @@ def home(request):
     base = request.POST.get('baseCurr') # get the base currency symbol from user
     counter = request.POST.get('counterCurr') # get the counter currency symbol from user
 
-    counterrate = 0
-    baserate = 0
-    totalAmount = 0
-    lastUpdated = 0
-    # Check if the base and counter is not none
-    # After requesting the server and getting data from user
-    if base != None and counter != None :
+    # # Check if the base and counter is not none
+    # # After requesting the server and getting data from user
 
-        json_urlcounter = urlopen("https://api.exchangeratesapi.io/latest?base=" + base)
-        json_urlbase = urlopen("https://api.exchangeratesapi.io/latest?base=" + counter)
+    json_url = urlopen("https://fcsapi.com/api-v2/forex/converter?pair1="+base+"&pair2="+counter+"&amount="+amount+"&access_key="+API_KEY)
+    data = json.loads(json_url.read())
 
-        counterdata = json.loads(json_urlcounter.read()) # fetching counter rates in the form of dict
-        basedata = json.loads(json_urlbase.read())       # fetching base rates in the form of dict
+    baseindex = "price_1x_"+base
+    counterindex = "price_1x_"+counter
 
-        # Getting the respective currency rate of counter currency
-        for i in counterdata['rates']:
-            if i == counter:
-                counterrate = counterdata['rates'].get(i)
-                break
+    baserate = data['response'][baseindex]
+    counterrate = data['response'][counterindex]
+    totalAmount = data['response']['total']
 
-        for j in basedata['rates']:
-            if j == base:
-                baserate = basedata['rates'].get(j)
-                break
-
-        totalAmount = float(amount) * counterrate  # Calculating total amount
-
-        lastUpdated = basedata['date']
-
-    Open, Close, High, Low = ohcl(base, counter)
+    High, Low = HighLow(base, counter)
+    lastUpd = lastUpdated(base, counter)
     context = {'totalAmount' : totalAmount,
                 'base' : base, 'counter' : counter,
                 'counterrate' : counterrate, 'baserate' : baserate,
-                'lastUpdated' : lastUpdated,
-                'Open' : Open, 'Close' : Close, 'High' : High, 'Low' : Low }
+                'lastUpd' : lastUpd,
+                'High' : High, 'Low' : Low }
     return render(request,'Base.html',context)
 
-def ohcl(base, counter):
-    if base != None and counter != None :
-        today = date.today()
-        todayDate = today.strftime("%Y-%m-%d")
-        fromDate = today - timedelta(30)
+def HighLow(base, counter):
+    today = date.today()
+    todayDate = today.strftime("%Y-%m-%d")
+    fromDate = today - timedelta(30)
 
-        # API_KEY = "taTjcoDno4fAXZKnSBLdvAEKonjHUq3FHdygpJiCwiRYdPKMhN"
-        # https://fcsapi.com/api-v2/forex/history?symbol=EUR/USD&period=1d&from=2020-05-01T12:00&to=2020-11-11T12:00&access_key=taTjcoDno4fAXZKnSBLdvAEKonjHUq3FHdygpJiCwiRYdPKMhN
-        json_url = urlopen("https://fcsapi.com/api-v2/forex/history?symbol="+base+"/"+counter+"&period=1d&from="+str(fromDate)+"T12:00&to="+todayDate+"T12:00&access_key=taTjcoDno4fAXZKnSBLdvAEKonjHUq3FHdygpJiCwiRYdPKMhN")
-        # json_url = urlopen("https://api.exchangeratesapi.io/latest?symbols=USD,GBP")
-        data = json.loads(json_url.read())
+    # https://fcsapi.com/api-v2/forex/history?symbol=EUR/USD&period=1d&from=2020-05-01T12:00&to=2020-11-11T12:00&access_key=taTjcoDno4fAXZKnSBLdvAEKonjHUq3FHdygpJiCwiRYdPKMhN
+    json_url = urlopen("https://fcsapi.com/api-v2/forex/history?symbol="+base+"/"+counter+"&period=1d&from="+str(fromDate)+"T12:00&to="+todayDate+"T12:00&access_key="+API_KEY)
+    data = json.loads(json_url.read())
 
-        dt = data['response']
+    dt = data['response']
 
-        opn = []
-        close = []
-        high = []
-        low = []
+    high = []
+    low = []
 
-        for ls in dt:
-            opn.append(ls['o'])
-            close.append(ls['c'])
-            high.append(ls['h'])
-            low.append(ls['l'])
+    for ls in dt:
+        high.append(ls['h'])
+        low.append(ls['l'])
 
-        return max(opn), max(close), max(high), max(low)
-    else:
-        return 0, 0, 0, 0
+    return max(high), min(low)
+
+def lastUpdated(base, counter):
+    json_url = urlopen("https://fcsapi.com/api-v3/forex/latest?symbol="+base+"/"+counter+"&access_key="+API_KEY)
+    data = json.loads(json_url.read())
+
+    return data['response'][0]['tm']
+    
