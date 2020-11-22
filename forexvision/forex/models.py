55c766f5563c61e7f3195a6f27d48109a36a1308
Ravi Agrawal <raviagrawal1012@gmail.com>
2020-11-22 12:11:38 +0530

    Market db improved

--- forexvision/forex/models.py
+++ forexvision/forex/models.py
@@ -5,6 +5,5 @@ class forex_hours(models.Model):
     country_name = models.CharField(max_length=50)
     open_time = models.TextField()
     close_time = models.TextField()
-    status = models.BooleanField()
+    status = models.CharField(max_length=50)
 
-    
